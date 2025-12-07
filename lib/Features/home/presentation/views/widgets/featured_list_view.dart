3df116a49c8c62eb5bae19503e07fc2b2c728f33
hp <mohamedoraby0500@gmail.com>
2025-12-07 20:11:52 +0200

    apply pagination on featured books list

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,22 +1,53 @@
 import 'package:clean_arch_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
+class FeaturedBooksListView extends StatefulWidget {
   const FeaturedBooksListView({super.key, required this.books});
   final List<BookEntity> books;
+
+  @override
+  State<FeaturedBooksListView> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  late final ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    scrollController.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    var currentPosition = scrollController.position.pixels;
+    var maxPostition = scrollController.position.maxScrollExtent;
+    if (currentPosition >= 0.7 * maxPostition) {
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
-        itemCount: books.length,
+        controller: scrollController,
+        itemCount: widget.books.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: CustomBookImage(image: books[index].image ?? ''),
+            child: CustomBookImage(image: widget.books[index].image ?? ''),
           );
         },
       ),
